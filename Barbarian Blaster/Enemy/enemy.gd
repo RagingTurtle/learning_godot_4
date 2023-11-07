b38493103bec6ef76ee77498f41723d15dff5809
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-11-07 11:17:27 -0600

    enemy health and damage

--- Barbarian Blaster/Enemy/enemy.gd
+++ Barbarian Blaster/Enemy/enemy.gd
@@ -1,7 +1,16 @@
 extends PathFollow3D
 
 @export var speed: float = 2.5
+@export var max_health: int = 50
 
+@onready var current_health: int = max_health:
+	get: 
+		return current_health 
+	set(value):
+		current_health = value
+		if current_health < 1:
+			self.queue_free()
+			
 @onready var base = get_tree().get_first_node_in_group("base")
 
 func _process(delta: float) -> void:
