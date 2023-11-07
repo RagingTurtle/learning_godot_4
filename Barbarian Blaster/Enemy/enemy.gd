4dc218c263a567f4acc3d6ca3ab0e363d0d0762a
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-11-07 09:26:01 -0600

    base

--- Barbarian Blaster/Enemy/enemy.gd
+++ Barbarian Blaster/Enemy/enemy.gd
@@ -2,11 +2,9 @@ extends PathFollow3D
 
 @export var speed: float = 2.5
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var base = get_tree().get_first_node_in_group("base")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	self.progress += delta * speed
+	if self.progress_ratio == 1.0:
+		base.take_damage()
