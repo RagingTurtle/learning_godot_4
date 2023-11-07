d0a9653dfd5becd04933bb00715da57d6eef5edb
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-11-07 11:32:29 -0600

    best target in range

--- Barbarian Blaster/Turret/turret.gd
+++ Barbarian Blaster/Turret/turret.gd
@@ -1,15 +1,30 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@export var turret_range: float = 10.0
 @onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
 var enemy_path: Path3D
+var target: PathFollow3D
 
 func _physics_process(delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	target = find_best_target()
+	if target != null:
+		look_at(target.global_position, Vector3.UP, true)
 	
 func _on_timer_timeout() -> void:
-	var shot: = projectile.instantiate()
-	add_child(shot)
-	shot.global_position = turret_top.global_position
-	shot.direction = global_transform.basis.z
+	if target:
+		var shot: = projectile.instantiate()
+		add_child(shot)
+		shot.global_position = turret_top.global_position
+		shot.direction = global_transform.basis.z
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress = 0
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			if self.global_position.distance_to(enemy.global_position) <= turret_range:
+				if enemy.progress > best_progress:
+					best_progress = enemy.progress
+					best_target = enemy
+	return best_target
