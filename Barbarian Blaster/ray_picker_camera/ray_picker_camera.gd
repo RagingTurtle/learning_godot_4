27e2ecf1d23738e0dbdfe7232bfb018a137bbbbf
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-11-07 10:08:53 -0600

    mouse raycast

---
+++ Barbarian Blaster/ray_picker_camera/ray_picker_camera.gd
@@ -0,0 +1,10 @@
+extends Camera3D
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+func _process(delta: float) -> void:
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100
+	ray_cast_3d.force_raycast_update()
+	print("collider: " + str(ray_cast_3d.get_collider()))
+	print("collider point: " + str(ray_cast_3d.get_collision_point()))
