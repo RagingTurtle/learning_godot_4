0a92d0761a6857321a3fbc0148ed2cde3a4f23ee
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-11-10 19:36:55 -0600

    difficulty manager

--- Barbarian Blaster/Enemy/enemy.gd
+++ Barbarian Blaster/Enemy/enemy.gd
@@ -2,6 +2,7 @@ extends PathFollow3D
 
 @export var speed: float = 2.5
 @export var max_health: int = 50
+@export var gold_reward: int = 15
 
 @onready var current_health: int = max_health:
 	get: 
@@ -12,8 +13,10 @@ extends PathFollow3D
 		current_health = value
 		if current_health < 1:
 			self.queue_free()
+			bank.gold += gold_reward
 			
 @onready var base = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _process(delta: float) -> void:
