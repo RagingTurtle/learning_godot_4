7f1a080e6c1c68734b0d0c4c18c58832e646f488
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-11-10 21:01:59 -0600

    new assets

--- Barbarian Blaster/Turret/turret.gd
+++ Barbarian Blaster/Turret/turret.gd
@@ -2,8 +2,10 @@ extends Node3D
 
 @export var projectile: PackedScene
 @export var turret_range: float = 10.0
-@onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
+@onready var turret_top: Node3D = $TurretBase/TurretTop
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var cannon: Node3D = $TurretBase/TurretTop/Cannon
+@onready var turret_base: Node3D = $TurretBase
 
 var enemy_path: Path3D
 var target: PathFollow3D
@@ -11,14 +13,14 @@ var target: PathFollow3D
 func _physics_process(_delta: float) -> void:
 	target = find_best_target()
 	if target != null:
-		look_at(target.global_position, Vector3.UP, true)
+		turret_base.look_at(target.global_position, Vector3.UP, true)
 	
 func _on_timer_timeout() -> void:
 	if target:
 		var shot: = projectile.instantiate()
 		add_child(shot)
-		shot.global_position = turret_top.global_position
-		shot.direction = global_transform.basis.z
+		shot.global_position = cannon.global_position
+		shot.direction = turret_base.global_transform.basis.z
 		animation_player.play("fire")
 
 func find_best_target() -> PathFollow3D:
