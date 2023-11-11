aa8d09d3ebf9fb9ca7daad89d047a84a078515f6
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-11-10 20:48:07 -0600

    victory rewards

--- Barbarian Blaster/UI/victory_layer.gd
+++ Barbarian Blaster/UI/victory_layer.gd
@@ -1,5 +1,22 @@
 extends CanvasLayer
 
+@onready var star_2: TextureRect = %Star2
+@onready var star_3: TextureRect = %Star3
+@onready var health_label: Label = %HealthLabel
+@onready var money_label: Label = %MoneyLabel
+
+@onready var base = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
+
+func victory() -> void:
+	visible = true
+	if base.max_health == base.current_health:
+		star_2.modulate = Color.WHITE
+		health_label.show()
+	if bank.gold >= 500:
+		star_3.modulate = Color.WHITE
+		money_label.show()
+
 func restart_game() -> void:
 	get_tree().reload_current_scene()
 	
